66bf0c44a4d036e9057bce6d2256904123eb1b3c
Rasheed <radiumrasheed@gmail.com>
2022-10-02 00:09:22 +0100

    mini x & o board game

--- main.go
+++ main.go
@@ -1,9 +1,25 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
+	"strings"
 )
 
 func main() {
-    fmt.Println("Hi")
+	// Create a tic-tac-toe board.
+	board := [][]string {
+		[]string{" ", " ", " "},
+		[]string{" ", " ", " "},
+		[]string{" ", " ", " "},
+	}
+
+	board[0][0] = "X"
+	board[2][0] = "0"
+	board[1][1] = "X"
+	board[2][1] = "0"
+	board[2][2] = "X"
+
+	for i := 0; i < len(board); i++ {
+		fmt.Println(strings.Join(board[i], ""))
+	}
 }
